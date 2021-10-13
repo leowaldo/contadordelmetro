44eea5e0c89dbb182b6c8c3af04bb5697e27c8c6
leowaldo <53881111+leowaldo@users.noreply.github.com>
2021-10-13 18:48:59 -0300

    Add files via upload

---
+++ MetroFreecode/index.js
@@ -0,0 +1,20 @@
+// document.getElementById("count-el").innerText=5 /* obtengo el elemento por id y le envio como parametro 5 
+
+let saveEl=document.getElementById("save-el")
+
+let countEl= document.getElementById("count-el");
+
+let contador=0;
+console.log(saveEl)
+
+function incF(){/* funcion del boton  incremento del contador */
+  contador+=1;
+  countEl.innerText=contador;
+}
+function saveF(){ /*funcion del boton guardar */
+ let countStr=contador+ " - " 
+  saveEl.innerText+=countStr;
+  console.log(contador)
+
+}
+saveF()
